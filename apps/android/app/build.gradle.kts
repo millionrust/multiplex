7375f746c774941460bbe5aca3eb240fcc15dd4d
terminoid <terminoid@millionrust.com>
2026-09-22 12:23:03 +0530

    build(release): 0.0.4

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.millionrust.multiplex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
         manifestPlaceholders["appLabel"] = "@string/app_name"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
